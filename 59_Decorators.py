6dbc446bdc3e757965564c9e2ac8ee7427d3f639
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-16 20:16:44 +0530

    Initial Commit

---
+++ 59_Decorators.py
@@ -0,0 +1,47 @@
+def greet(fx):
+  def mfx():
+    print("Good Morning")
+    fx()
+    print("Thanks for using this function")
+  return mfx
+
+# @greet
+def hello():
+  print("Hello world")
+  
+# hello()
+greet(hello)()
+
+print("\n")
+
+def greet(fx):
+  def mfx(*args, **kwargs):
+    print("Good Morning")
+    fx(*args, **kwargs)
+    print("Thanks for using this function")
+  return mfx
+
+# @greet
+def add(a, b):
+  print(a+b)
+
+# add(1, 2)
+greet(add)(1, 2)
+
+print("\n")
+
+import logging
+
+def log_function_call(func):
+    def decorated(*args, **kwargs):
+        logging.info(f"Calling {func.__name__} with args={args}, kwargs={kwargs}")
+        result = func(*args, **kwargs)
+        logging.info(f"{func.__name__} returned {result}")
+        return result
+    return decorated
+
+@log_function_call
+def my_function(a, b):
+    return a + b
+
+print(my_function(1,2))
