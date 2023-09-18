97d3e2e1b1e0eae4d19e83339bbb008554ee56fc
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-18 16:35:01 +0530

    Initial Commit

---
+++ 61_Inheritance.py
@@ -0,0 +1,21 @@
+class Employee:
+  def __init__(self, name, id):
+    self.name = name
+    self.id = id 
+
+  def showDetails(self):
+    print(f"The name of Employee: {self.id} is {self.name}")
+
+class Programmer(Employee):
+  def showLanguage(self):
+    print("The default langauge is Python")
+
+
+e1 = Employee("Rohan Das", 400)
+e1.showDetails()
+e2 = Employee("Harry", 4100)
+e2.showDetails()
+#e2.showLanguage() # error occurs as showLanguage is not the part of Employee class
+p1 = Programmer("Lokesh",7)
+p1.showDetails()
+p1.showLanguage()
