e4af7168bb54dfbeb99697b8ff245d213e985423
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-30 10:37:27 +0530

    Initial Commit

--- Python_Exercises/83_Ex_9_shoutoutsToEveryone.py
+++ Python_Exercises/83_Ex_9_shoutoutsToEveryone.py
@@ -11,4 +11,27 @@
 # Note: If you are not using windows, 
 # try to figure out how to do the same thing using some other package
 
+# importing the pyttsx library
+import pyttsx3
+import time # for using time.sleep(x)
 
+# initialisation
+engine = pyttsx3.init()
+
+# testing
+engine.say("My first code on text-to-speech")
+
+engine.runAndWait() # to run the speech we use runAndWait() 
+
+# Shoutouts to given names in the lists
+
+names = ["Lokesh Rao Sahab", "Shivam Rao Sahab", "Shivam Tyagi", "Ankit Bhai", "Kadambdi Nikhil", "Aman Tyagi"]
+
+for name in names :
+    engine.say(f"Shoutout to {name}")
+
+engine.runAndWait()
+
+time.sleep(2)
+engine.say("Thanks for using this")
+engine.runAndWait()
