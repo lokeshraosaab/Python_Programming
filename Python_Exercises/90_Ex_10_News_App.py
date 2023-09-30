f01ac45bedf8bb7d8fae26d95a8fe03cdc8e1945
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-09-30 15:07:10 +0530

    Initial Commit

--- Python_Exercises/90_Ex_10_News_App.py
+++ Python_Exercises/90_Ex_10_News_App.py
@@ -2,3 +2,20 @@
 # Use the NewsAPI and the requests module to fetch the daily news related to different topics.
 #  Go to: https://newsapi.org/ and explore the various options to build you application
 
+import requests
+import json
+
+query = input("What type of news are you interested in : ")
+url = f"https://newsapi.org/v2/everything?q={query}&from=2023-08-30&sortBy=publishedAt&apiKey=20e36941da894afebc590e5773b9cca4"
+response = requests.get(url)
+
+# print(response.text)
+
+news = json.loads(response.text)
+
+# print(news,type(news))
+
+for article in news["articles"] :
+    print(article["title"])
+    print(article["description"])
+    print("-------------------------------------------")
