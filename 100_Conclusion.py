cdd1deb3f5a128254858b7825605b201cd895b2c
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-01 16:07:36 +0530

    Initial Commit

---
+++ 100_Conclusion.py
@@ -0,0 +1,9 @@
+# Link to some resources
+
+# Tkinter - You can learn Tkinter which is used to create GUIs from here :
+# Machine Learning - I loved this playlist from Google Developers
+# Django - For Django, try the tutorial from the official documentation. Trust me its really good
+
+# Overall, the key to mastering Python (or any programming language) is to keep practicing 
+# and experimenting. Set yourself challenges, work on personal projects, and 
+# stay curious. Good luck!
