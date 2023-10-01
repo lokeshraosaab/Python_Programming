0db8ac4dc6eaded61f2fc537cc246dc3dc37a70f
Lokesh Kumar <lokeshkumar797877@gmail.com>
2023-10-01 16:14:21 +0530

    Excercise Done

--- Python_Exercises/94_Ex_11_Drink_Water_Reminder.py
+++ Python_Exercises/94_Ex_11_Drink_Water_Reminder.py
@@ -2,3 +2,17 @@
 # Write a python program which reminds you of drinking water every hour or two. 
 # Your program can either beep or send desktop notifications for a specific operating system
 
+import time
+import pyttsx3
+from plyer import notification
+
+
+engine = pyttsx3.init()
+Reminder_time = float(input("tell the time after which you will drink water : "))
+while(True):
+    time.sleep(Reminder_time)
+    engine.say("You Should Drink Water")
+    notification.notify(title = "Notification",
+                        message = "You should drink water",
+                        timeout = 5)
+    engine.runAndWait()
